555e6c2510076ba84624ff627d05d1056cbceae9
Godwin <138124430+ckdonah@users.noreply.github.com>
2024-08-01 11:25:34 +0200

    projectcard

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -1,4 +1,5 @@
-// icons
+import React from 'react';
+// Icons
 import { IoPlaySharp } from "react-icons/io5";
 import { BsGithub } from "react-icons/bs";
 
@@ -11,42 +12,38 @@ const ProjectCard = ({
   demoUrl,
   codeUrl,
 }) => {
-  const btnStyle = "flex items-center justify-center gap-2 border-2 border-solid p-1 text-base font-[500] transition-all duration-200 ease-in-out hover:bg-green-light hover:text-blue-main hover:shadow-none active:bg-green-dark active:text-white-main active:shadow-none bg-gray-main border-black-main shadow-box-shadow";
+  const btnStyle = "bg-black text-white py-1.5 px-3 rounded-md transition-colors duration-200 ease-in-out hover:bg-gray-800 flex items-center justify-center gap-1";
 
   return (
-    <>
-      <article>
-        <div className="m-[10px] h-[430px] w-[300px] rounded overflow-hidden border-black-main border-0 bg-gray-main shadow-box-shadow">
-          <a href={imageUrl} target="_blank" rel="noopener noreferrer">
-            <img
-              src={image}
-              alt={altTitle}
-              className="mb-2 h-[200px] w-[300px] object-cover object-center transition-all duration-200 ease-in-out hover:contrast-50"
-            />
-            <h3 className="py-1 text-center text-lg font-[700] uppercase decoration-2 transition-all duration-200 ease-in-out hover:underline">
-              {title}
-            </h3>
-          </a>
-          <p className="mx-2 h-28 p-1">{description}</p>
-
-          <div className="flex justify-between px-12 py-4">
+    <article className="project small">
+      <div className="rounded overflow-hidden">
+        <a href={imageUrl} target="_blank" rel="noopener noreferrer">
+          <img
+            src={image}
+            alt={altTitle}
+            className="mb-2 w-full h-auto object-cover object-center transition-all duration-200 ease-in-out hover:contrast-50"
+          />
+          <h3 className="py-2 text-center text-base font-bold uppercase transition-all duration-200 ease-in-out hover:underline">
+            {title}
+          </h3>
+        </a>
+        <p className="p-2 text-center text-bg">{description}</p>
+        <div className="flex justify-between px-2 py-2">
+          <div className="w-fit">
+            <a href={codeUrl} target="_blank" rel="noopener noreferrer" className={btnStyle}>
+              Code <BsGithub />
+            </a>
+          </div>
+          {demoUrl && (
             <div className="w-fit">
-              <a href={codeUrl} target="_blank" rel="noopener noreferrer" className={btnStyle}>
-                Code <BsGithub />
+              <a href={demoUrl} target="_blank" rel="noopener noreferrer" className={btnStyle}>
+                Live Demo <IoPlaySharp />
               </a>
             </div>
-
-            {demoUrl && (
-              <div className="w-fit">
-                <a href={demoUrl} target="_blank" rel="noopener noreferrer" className={btnStyle}>
-                  Live Demo <IoPlaySharp />
-                </a>
-              </div>
-            )}
-          </div>
+          )}
         </div>
-      </article>
-    </>
+      </div>
+    </article>
   );
 };
 
