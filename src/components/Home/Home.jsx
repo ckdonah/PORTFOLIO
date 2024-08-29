cb66adde31fba8695fdbaa2dbc3765160415da53
Godwin <138124430+ckdonah@users.noreply.github.com>
2024-08-29 20:40:29 +0200

    powerhour

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,44 @@
-// Home.js
 import React from "react";
-import Projects from "../Project/Projects"; // Import the Projects component
+import Projects from "../Project/Powerhour";
 import "./Home.css";
 import handIcon from "../../assets/icons/hand-icon.png";
 
 function Home() {
   return (
-    <main className="main">
-      {/* Welcome Section */}
-      <div className="welcome-section" id="welcome">
+    <div className="main">
+      <section className="welcome-section" id="home">
         <div className="welcome-icon-container">
           <img src={handIcon} alt="Hand Icon" className="welcome-icon" />
         </div>
         <div className="welcome-text">
           <h2>Hi there!</h2>
           <p>
-            I'm Godwin Daniel, a Software Developer <br /> specializing in building web applications.
+            I'm Godwin Daniel, a Software Developer <br /> specializing in
+            building web applications.
           </p>
         </div>
+      </section>
+      <div className="arrow-down">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="26"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-chevron-down"
+        >
+          <polyline points="6 9 12 15 18 9"></polyline>
+        </svg>
       </div>
-      <hr className="content-divider" /> {/* Add this line */}
-
-      
-      <div className="projects-content" id="projects">
-        <Projects /> 
-      </div>
-    </main>
+      <hr className="content-divider" />
+      <section className="projects-content" id="projects">
+        <Projects />
+      </section>
+    </div>
   );
 }
 
